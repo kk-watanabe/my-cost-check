3295ef6a4abb98881cddadb4efae86d21d109e85
kk-watanabe <kazuki.watanabe@bebit.com>
2021-07-14 20:22:06 +0900

    fix: Fix test setting

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   transform: {
+    "^.+\\.svg$": "<rootDir>/tests/unit/_mock/svg-transform.js",
     "^.+\\.vue$": "vue-jest",
   },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@test-utils/(.*)$": "<rootDir>/tests/unit/_helpers/$1",
+  },
+  testMatch: ["**/tests/unit/**/*.spec.(js|jsx|ts|tsx)"],
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.vue",
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/stories/**/*.ts",
+    "!<rootDir>/src/shims-tsx.d.ts",
+    "!<rootDir>/src/shims-vue.d.ts",
+  ],
 };
