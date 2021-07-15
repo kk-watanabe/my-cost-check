9d6e36ea9b779d882a3f3dac5ef1b19023f95133
kk-watanabe <kazuki.watanabe@bebit.com>
2021-07-15 16:00:29 +0900

    feat: Add Panel components

---
+++ src/components/atoms/Panel.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="panel"><slot /></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Panel",
+});
+</script>
+
+<style scoped lang="scss">
+.panel {
+  padding: 30px;
+  border-radius: $borderRadiusLarge;
+  background-color: $isColorWhite;
+  box-shadow: 1px 1px 5px rgba($isColorBase5, 0.5);
+  line-height: 1;
+  transition: box-shadow 0.3s ease;
+
+  &:hover {
+    box-shadow: 2px 2px 5px rgba($isColorBase5, 0.6);
+  }
+}
+</style>
