02b37689c12ec34c3cff0d8376631f8102062051
zukki30 <kw4869ify@gmail.com>
2022-04-08 01:14:18 +0900

    feat: Add eslint & prettier

--- vue/.storybook/main.js
+++ vue/.storybook/main.js
@@ -1,36 +1,28 @@
 const path = require("path");
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   typescript: {
     check: false,
     checkOptions: {},
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: prop =>
-        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true
-    }
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
   },
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     config.resolve.alias = {
-      "vue": "vue/dist/vue.esm-bundler.js",
+      vue: "vue/dist/vue.esm-bundler.js",
       "@": path.resolve(__dirname, "../src"),
-      "@test-utils": path.resolve(__dirname, "../tests/unit/_helpers")
+      "@test-utils": path.resolve(__dirname, "../tests/unit/_helpers"),
     };
 
     config.module.rules.push({
       test: /\.js$/,
-      use: [
-        "babel-loader",
-      ]
+      use: ["babel-loader"],
     });
 
     config.module.rules.push({
@@ -51,13 +43,13 @@ module.exports = {
             resources: [
               path.resolve(__dirname, "../src/assets/scss/_color.scss"),
               path.resolve(__dirname, "../src/assets/scss/_size.scss"),
-              path.resolve(__dirname, "../src/assets/scss/_functions.scss")
-            ]
-          }
-        }
-      ]
+              path.resolve(__dirname, "../src/assets/scss/_functions.scss"),
+            ],
+          },
+        },
+      ],
     });
 
     return config;
-  }
-}
+  },
+};
