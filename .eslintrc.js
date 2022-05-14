8c3934c4eb8f9a7c833aff351eeebfd3678e07b8
zukki30 <kw4869ify@gmail.com>
2022-05-15 04:08:02 +0900

    feat: Add eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["node_modules/*"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -13,5 +25,29 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": ["error"],
+    "@typescript-eslint/no-explicit-any": ["error"],
+    "react-hooks/exhaustive-deps": ["error"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/ignore": "node_modules",
+    "import/resolver": {
+      typescript: { project: "./" },
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
 };
