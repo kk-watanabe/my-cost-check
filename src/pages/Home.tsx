d8b1ef6d79c43d4576c4b309b133a9bfd1ce6238
zukki30 <kw4869ify@gmail.com>
2022-05-18 03:21:25 +0900

    feat: Add Firebase data get methods

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,32 @@
-import { VFC } from "react";
+import { VFC, useCallback, useEffect } from "react";
 import { useFirebaseContext } from "@/providers/FirebaseProvider";
 import { useUserContext } from "@/providers/UserProvider";
+import { buildUserInfo, buildCosts } from "@/utils/type-utils";
 
 const Home: VFC = () => {
-  const { auth } = useFirebaseContext();
-  const { user } = useUserContext();
+  const { getQuerySnapshot, getDocumentSnapshot } = useFirebaseContext();
+  const { uid } = useUserContext();
+  const usersCollectionName = "users";
+  const usersPath = `${usersCollectionName}/${uid}`;
+  const costsCollectionName = "costs";
+  const costsPath = `${usersPath}/${costsCollectionName}`;
 
-  console.log(auth, user);
+  const fetch = useCallback(async () => {
+    const userResult = getDocumentSnapshot(usersPath);
+
+    if ((await userResult).exists()) {
+      const userInfo = buildUserInfo((await userResult).data() as UserDocuments);
+      console.log(userInfo);
+
+      const costsResult = getQuerySnapshot(costsPath);
+      const costs = await (await costsResult).docs.map(buildCosts);
+      console.log(costs);
+    }
+  }, [getDocumentSnapshot, usersPath, getQuerySnapshot, costsPath]);
+
+  useEffect(() => {
+    fetch();
+  }, [fetch]);
 
   return <div>Home</div>;
 };
