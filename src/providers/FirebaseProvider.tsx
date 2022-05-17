fe4e98c2aa83ef18677e136a1d107b24fc93e7f3
zukki30 <kw4869ify@gmail.com>
2022-05-18 02:48:22 +0900

    feat: Add getQuerySnapshot & getDocumentSnapshot

--- src/providers/FirebaseProvider.tsx
+++ src/providers/FirebaseProvider.tsx
@@ -3,10 +3,24 @@ import React from "react";
 import { initializeApp } from "firebase/app";
 import { getAnalytics, Analytics } from "firebase/analytics";
 import { getAuth, browserLocalPersistence, Auth } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  QueryConstraint,
+  doc,
+  getDoc,
+  QuerySnapshot,
+  DocumentData,
+  DocumentSnapshot,
+} from "firebase/firestore";
 
 type FirebaseContextState = {
   analytics: Analytics;
   auth: Auth;
+  getQuerySnapshot: (path: string, ...queryConstraints: QueryConstraint[]) => Promise<QuerySnapshot<DocumentData>>;
+  getDocumentSnapshot: (path: string, ...pathSegments: string[]) => Promise<DocumentSnapshot<DocumentData>>;
 };
 
 const FirebaseContext = React.createContext({} as FirebaseContextState);
@@ -32,8 +46,18 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 auth.setPersistence(browserLocalPersistence);
 
+const firestore = getFirestore(app);
+
 export const FirebaseProvider = ({ children }: { children: React.ReactNode }) => {
-  const value = { analytics, auth };
+  const getQuerySnapshot = async (path: string, ...queryConstraints: QueryConstraint[]) => {
+    return await getDocs(query(collection(firestore, path), ...queryConstraints));
+  };
+
+  const getDocumentSnapshot = async (path: string, ...pathSegments: string[]) => {
+    return await getDoc(doc(firestore, path, ...pathSegments));
+  };
+
+  const value = { analytics, auth, getQuerySnapshot, getDocumentSnapshot };
 
   return <FirebaseContext.Provider value={value}>{children}</FirebaseContext.Provider>;
 };
