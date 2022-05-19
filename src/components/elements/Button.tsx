f5e1a6c6cefa9e458b6aaf32fcd4f69998ffacf4
zukki30 <kw4869ify@gmail.com>
2022-05-20 02:43:46 +0900

    feat: Add Button components

---
+++ src/components/elements/Button.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import tw from "tailwind-styled-components";
+
+type ButtonSize = "small" | "medium" | "large";
+
+type ButtonType = "primary" | "secondary" | "error" | "warning";
+
+export interface ButtonProps {
+  label: string;
+  type?: ButtonType;
+  size?: ButtonSize;
+  isDisabled?: boolean;
+  fullWidth?: boolean;
+  onClick?: () => void;
+}
+
+const ButtonBase = tw.button`
+  flex
+  items-center
+  justify-center
+  border
+  border-solid
+
+  ${(props: ButtonProps) => {
+    const { type, isDisabled, size, fullWidth } = props;
+    const result = [];
+
+    // Border Radius
+    if (size === "small") {
+      result.push("rounded");
+    } else if (size === "large") {
+      result.push("rounded-lg");
+    } else {
+      result.push("rounded-md");
+    }
+
+    // font-size
+    if (size === "small") {
+      result.push("text-sm");
+    } else if (size === "large") {
+      result.push("text-lg");
+    } else {
+      result.push("text-base");
+    }
+
+    // width
+    if (fullWidth) {
+      result.push("w-full");
+    } else if (size === "small") {
+      result.push("w-40");
+    } else if (size === "large") {
+      result.push("w-64");
+    } else {
+      result.push("w-52");
+    }
+
+    // height
+    if (size === "small") {
+      result.push("h-10");
+    } else if (size === "large") {
+      result.push("h-12");
+    } else {
+      result.push("h-11");
+    }
+
+    // border-color & bg-color
+    if (isDisabled) {
+      result.push("bg-slate-500", "border-slate-500", "text-slate-300");
+    } else if (type === "secondary") {
+      result.push("bg-lime-500", "hover:bg-lime-400", "border-lime-500", "hover:border-lime-400", "text-white");
+    } else if (type === "error") {
+      result.push("bg-red-600", "hover:bg-red-500", "border-red-600", "hover:border-red-500", "text-white");
+    } else if (type === "warning") {
+      result.push("bg-amber-500", "hover:bg-amber-400", "border-amber-500", "hover:border-amber-400", "text-white");
+    } else {
+      result.push("bg-sky-500", "hover:bg-sky-400", "border-sky-500", "hover:border-sky-400", "text-white");
+    }
+
+    // when isDisabled
+    if (isDisabled) {
+      result.push("pointer-events-none");
+    } else {
+      result.push("font-bold", "transition");
+    }
+
+    return result.join(" ");
+  }}
+`;
+
+export const Button = ({
+  type = "primary",
+  size = "medium",
+  isDisabled = false,
+  fullWidth = false,
+  ...props
+}: ButtonProps) => {
+  const { label } = props;
+
+  return (
+    <ButtonBase type={type} size={size} isDisabled={isDisabled} fullWidth={fullWidth} {...props}>
+      {label}
+    </ButtonBase>
+  );
+};
---
+++ src/stories/elements/Button.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Button } from "@/components/elements/Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "components/elements/Button",
+  component: Button,
+  argTypes: {
+    type: {
+      options: ["primary", "secondary", "error", "warning"],
+      control: { type: "radio" },
+    },
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "radio" },
+    },
+    isDisabled: { control: "boolean" },
+    fullWidth: { control: "boolean" },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  label: "Button",
+  type: "primary",
+  size: "medium",
+  isDisabled: false,
+  fullWidth: false,
+};
