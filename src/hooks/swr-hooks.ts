6fd81a4ea8947f0337d177be7c07283dbcf4f1ad
zukki30 <kw4869ify@gmail.com>
2022-05-26 03:41:49 +0900

    feat: Add swr-hooks

---
+++ src/hooks/swr-hooks.ts
@@ -0,0 +1,31 @@
+import useSWR from "swr";
+import { collection, getDocs, query } from "firebase/firestore";
+import { firestore } from "@/providers/FirebaseProvider";
+import { buildCosts } from "@/utils/type-utils";
+
+const useFetcher = async (url: string) => {
+  const costsResult = await getDocs(query(collection(firestore, url)));
+
+  return costsResult.docs.map(buildCosts);
+};
+
+/**
+ * useSWR を使用する
+ * @returns
+ */
+export const useFetchCostsWithSWR = (url: string) => {
+  const {
+    data,
+    error,
+    mutate: setCosts,
+  } = useSWR(url, useFetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+  const costs = data === undefined ? [] : data;
+  const isLoading = !error && !data;
+  const isError = error;
+
+  return { costs, isLoading, isError, setCosts };
+};
